bd86d20ca9fb19383236229186c465a2ba0f6e15
Patrik <128815730+patriklts@users.noreply.github.com>
2025-01-25 19:40:15 +0100

    added comments

--- src/main.py
+++ src/main.py
@@ -7,38 +7,42 @@ import os
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Constants
+# String constants for the room status
 ROOM_AVAILABLE_STR = "Raum B-0.270 ist frei"
 ROOM_UNAVAILABLE_STR = "Raum B-0.270 ist belegt"
 
-# Kafka topic
+# Kafka room status topic from environment variable
 KAFKA_ROOM_STATUS_TOPIC = os.getenv("KAFKA_ROOM_STATUS_TOPIC", "room_status")
 
-# Function to get the room status from the message
-def get_room_status(message):
-    logging.info(f"Received message: {message}")
-    
-    payload_str = message.value["message"]
-    data = json.loads(payload_str)
+
+def get_room_status(message) -> bool:
+    """Get the new room status from the received message. Return the new status as a boolean"""
+    logging.info(f"Received message: {message.topic} -> {message.value}")
+
+    # Get the button value from the message and rerturn the new status
+    data = json.loads(message.value["message"])
     return data.get("button_toggled")
 
 
 def get_kafka_consumer():
+    """Get a Kafkaconfig instance and subscribe a consumer to the topics of the constant"""
     kafka_config = KafkaConfig()
     consumer = setup_kafka_consumer(kafka_config, [KAFKA_ROOM_STATUS_TOPIC])
     return consumer
 
+
 def main():
     # Set up the Kafka consumer
     consumer = get_kafka_consumer()
-    
-    # Set up the Streamlit app
+
+    # Set up the Streamlit app for the room status display
     st.title("Verfügbare Räume")
 
     # Create a placeholder for the status message and display the initial status
     status_placeholder = st.empty()
     status_placeholder.success(ROOM_AVAILABLE_STR, icon="✅")
 
+    # If the consumer gets a new message, update the status message, depending on the room status
     for message in consumer:
         if get_room_status(message):
             status_placeholder.success(ROOM_AVAILABLE_STR, icon="✅")
