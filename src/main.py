56d72de0a8590a8bc4425f5e178bc77c7159a79d
Patrik <128815730+patriklts@users.noreply.github.com>
2025-01-27 13:32:10 +0100

    Fix room status retrieval to access button_toggled from message.value

--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ def get_room_status(message) -> bool:
     logging.info(f"Received message: {message.topic} -> {message.value}")
 
     # Get the button value from the message and rerturn the new status
-    return message["button_toggled"]
+    return message.value["button_toggled"]
 
 
 def get_kafka_consumer():
